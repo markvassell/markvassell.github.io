4e69272a1c8e3be4865f06aecd56babf6e53c250
markvassell <mdvy96@mail.missouri.edu>
2016-02-23 11:11:46 -0600

    added html resume

--- functions/functions.js
+++ functions/functions.js
@@ -1,19 +1,19 @@
 
                 function home(){
-                        document.getElementById("info").innerHTML='<object class = "text" type="text/html" data="content/home.php" ></object>';
+                        document.getElementById("info").innerHTML='<object class = "text" type="text/html" data="content/home.html" ></object>';
                 }
                 function contact(){
-                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/contact.php" ></object>';
+                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/contact.html" ></object>';
                 }
                 function work(){
-                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/work.php" ></object>';
+                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/work.html" ></object>';
                 }
                 function school(){
-                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/school.php" ></object>';
+                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/school.html" ></object>';
                 }
                 function accomplish(){
-                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/accomplishment.php" ></object>';
+                        document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/accomplishment.html" ></object>';
                 }
                 function experience(){
-                		document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/experience.php" ></object>';
+                		document.getElementById("info").innerHTML='<object type="text/html" class = "text" data="content/experience.html" ></object>';
                 }
